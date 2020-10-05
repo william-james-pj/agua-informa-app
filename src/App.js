435d296c0ad91756ea806724b144665444bc76ae
William James <52788612+william-james-pj@users.noreply.github.com>
2020-10-05 15:49:06 -0300

    Configuração Inicial

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
 import React from 'react';
 import {StyleSheet, View, Text} from 'react-native';
 
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+
+import BemVindoScreen from './screens/BemVindoScreen';
+import LoginScreen from './screens/LoginScreen';
+import SignUpScreen from './screens/SignUpScreen';
+import HomeScreen from './screens/HomeScreen';
+
+const Stack = createStackNavigator();
+
 const App = () => {
   return (
-    <View>
-      <Text>AAa</Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="BemVindo">
+        <Stack.Screen
+          name="BemVindo"
+          component={BemVindoScreen}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name="Login"
+          component={LoginScreen}
+          options={{headerTitle: false, headerTintColor: '#70D1D3'}}
+        />
+        <Stack.Screen
+          name="SignUp"
+          component={SignUpScreen}
+          options={{headerTitle: false, headerTintColor: '#70D1D3'}}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
--- src/screens/BemVindoScreen.js
+++ src/screens/BemVindoScreen.js
@@ -1,14 +1,80 @@
 import React from 'react';
-import {StyleSheet, View, Text} from 'react-native';
+import { StyleSheet, Text, View, KeyboardAvoidingView } from 'react-native';
 
-const BemVindoScreen = () => {
+import ButtonCustom from '../components/ButtonCustom';
+
+function BemVindoScreen({navigation}) {
   return (
-    <View>
-      <Text>BemVindoScreen</Text>
-    </View>
+    <KeyboardAvoidingView style={styles.containerStyle}>
+      <View style={styles.containerText}>
+        <Text style={styles.titleStyle}>Bem-vindo</Text>
+        <Text style={styles.subtitleStyle}>
+          Faça login ou inscreva-se para continuar
+        </Text>
+        <Text style={styles.subtitleStyle}>usando nosso aplicativo</Text>
+      </View>
+      <View style={styles.containerImg}>
+        <View style={styles.imgStyle} />
+      </View>
+      <View style={styles.containerButton}>
+        <ButtonCustom
+          testValue={'Conecte-se'}
+          functionOnPress={() => navigation.navigate('Login')}
+          // eslint-disable-next-line react-native/no-inline-styles
+          parametosStyle={{ColorFundo: '#70D1D3'}}
+        />
+        <ButtonCustom
+          testValue={'Criar nova conta'}
+          functionOnPress={() => navigation.navigate('SignUp')}
+          colorText={'#70D1D3'}
+          // eslint-disable-next-line react-native/no-inline-styles
+          parametosStyle={{
+            ColorFundo: '#fff',
+            widthBorder: 1,
+            colorBorder: '#70D1D3',
+            TopMargin: 20,
+          }}
+        />
+      </View>
+    </KeyboardAvoidingView>
   );
-};
+}
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  containerStyle: {
+    width: '100%',
+    height: '100%',
+    backgroundColor: '#fff',
+  },
+  containerText: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+  },
+  titleStyle: {
+    fontSize: 26,
+    color: '#70D1D3',
+  },
+  subtitleStyle: {
+    fontSize: 10,
+    color: '#C4C4C4',
+  },
+  containerImg: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  imgStyle: {
+    width: '60%',
+    height: '80%',
+    backgroundColor: '#C4C4C4',
+    borderRadius: 20,
+  },
+  containerButton: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
 
 export default BemVindoScreen;
